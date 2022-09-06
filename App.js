321fa1a860025879cc2722cb98f33932163ac7d4
Nalin <nalinssn@gmail.com>
2022-09-06 12:44:30 +0530

    implement image picker

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   Text,
@@ -8,9 +8,41 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import * as ImagePicker from "expo-image-picker";
+
 export default function App() {
+  useEffect(() => {
+    setImageUrl("https://i.imgur.com/TkIrScD.png");
+  }, []);
+
+  const [imageUrl, setImageUrl] = useState();
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsMultipleSelection: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (result?.uri) {
+      setImageUrl(result.uri);
+    }
+    if (!result.cancelled) {
+      setImageUrl(result.uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
+      <TouchableOpacity onPress={pickImage}>
+        <Image
+          source={{ uri: imageUrl }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+      </TouchableOpacity>
       <TouchableOpacity
         style={styles.buttonContainer}
         onPress={() => {
@@ -31,10 +63,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   buttonContainer: {
+    marginTop: 30,
     width: 200,
     height: 50,
     borderWidth: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  image: {
+    width: 300,
+    height: 300,
   },
 });
